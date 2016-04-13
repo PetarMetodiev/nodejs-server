7f8648c9b2a79a01064ee7f28ffbec4569bd8ea6
Petar Metodiev <petar.metodiev@gmail.com>
2016-04-13 08:21:04 +0300

    Removed unnecessary bloat in the home controller.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,26 +1,18 @@
-var express = require('express'),
-	router = express.Router(),
-	mongoose = require('mongoose'),
-	Article = mongoose.model('Article');
+var express = require('express')
+	, router = express.Router()
+	, mongoose = require('mongoose')
+	, Article = mongoose.model('Article');
 
-module.exports = function(app) {
+module.exports = function (app) {
 	app.use('/', router);
 };
 
-router.get('/', function(req, res, next) {
-	Article.find(function(err, articles) {
+router.get('/', function (req, res, next) {
+	Article.find(function (err, articles) {
 		if (err) return next(err);
 		res.render('index', {
-			title: 'Generator-Express MVC',
-			articles: articles
+			title: 'Generator-Express MVC'
+			, articles: articles
 		});
 	});
-});
-
-router.get('/test', function(req, res, next) {
-	res.send('Test hello!');
-});
-
-router.get('/show-off', function(req, res, next) {
-	res.render('miro');
 });
